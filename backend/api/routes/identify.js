1d8bdf1004276db51cc03e2b3b0021a2b971eaee
Christian Wende <christian.wende96@hotmail.de>
2022-01-25 23:45:41 +0100

    changed response messages to a more user-friendly version

--- backend/api/routes/identify.js
+++ backend/api/routes/identify.js
@@ -6,27 +6,29 @@ require('dotenv').config()
 
 router.post("/", (req, res, next) => {
 
-    if (!req.body.URL) res.status(500).json({message: "please specify an URL"})
-    
-    const baseURl =  `https://my-api.plantnet.org/v2/identify/`
+    if (!req.body.URL) res.status(500).json({ message: "Es wurde keine URL angegeben." })
+
+    const baseURL = `https://my-api.plantnet.org/v2/identify/`
     const imagesURL = encodeURIComponent(req.body.URL)
-    const sendURL = `${baseURl}all?api-key=${process.env.PLANT_API_KEY}&images=${imagesURL}`
+    const sendURL = `${baseURL}all?api-key=${process.env.PLANT_API_KEY}&images=${imagesURL}`
 
     axios({
-        method: 'get',
-        url:sendURL,
-      })
-        .then(function (response) {
+            method: 'get',
+            url: sendURL,
+        })
+        .then(function(response) {
             const resultCount = 3
             const sendResponse = []
             if (response.data.results.length < 3) resultCount = response.data.results.length
             for (let i = 0; i < resultCount; i++) {
-                sendResponse.push({ score : response.data.results[i].score,
-                                    species: response.data.results[i].species.scientificNameWithoutAuthor})
+                sendResponse.push({
+                    score: response.data.results[i].score,
+                    species: response.data.results[i].species.scientificNameWithoutAuthor
+                })
             }
             res.status(200).json(sendResponse)
         })
-        .catch((error)=>{console.log(error)});
-  })
- 
+        .catch((error) => { console.log(error) });
+})
+
 module.exports = router
