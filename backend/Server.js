1a2f2e58000e5646f54dee5634a95d53193a040e
blank854 <blank854@gmail.com>
2022-01-28 08:23:35 +0100

    as discussed with timo

--- backend/Server.js
+++ backend/Server.js
@@ -1,42 +1,46 @@
-const Express = require("express");
-const expressServer = Express();
-const offerRoutes = require("./api/routes/offer");
-const userRoutes = require("./api/routes/user");
-const uploadRoutes = require("./api/routes/upload");
-const chatRoutes = require("./api/routes/chat");
-const identifyRoutes = require("./api/routes/identify");
-const bodyParser = require("body-parser");
-const Mongoose = require("mongoose");
-const statisticsRoutes = require("./api/routes/statistics");
-const clean = require("./clean")
-const CronJob = require('cron').CronJob;
+const Express = require('express')
+const expressServer = Express()
+const offerRoutes = require('./api/routes/offer')
+const userRoutes = require('./api/routes/user')
+const uploadRoutes = require('./api/routes/upload')
+const chatRoutes = require('./api/routes/chat')
+const identifyRoutes = require('./api/routes/identify')
+const bodyParser = require('body-parser')
+const Mongoose = require('mongoose')
+const statisticsRoutes = require('./api/routes/statistics')
+const clean = require('./clean')
+const CronJob = require('cron').CronJob
 require('dotenv').config()
 
-Mongoose.connect(process.env.MONGODBACCESS).catch(err => { console.log("Error") });
+Mongoose.connect(process.env.MONGODBACCESS).catch((err) => {
+  console.log('Error')
+})
 
-//expressServer.use(bodyParser.urlencoded({ extended: true }));
-expressServer.use(bodyParser.json());
+expressServer.use(bodyParser.urlencoded({ extended: true }))
+expressServer.use(bodyParser.json())
 
 expressServer.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Headers',
-        'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE')
-        return res.status(200).json({});
-    }
-    next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  )
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, GET, DELETE')
+    return res.status(200).json({})
+  }
+  next()
+})
 
-expressServer.use('/offer', offerRoutes);
-expressServer.use('/user', userRoutes);
-expressServer.use('/upload', uploadRoutes);
-expressServer.use('/identify', identifyRoutes);
-expressServer.use('/chat', chatRoutes);
-expressServer.use('/statistics', statisticsRoutes);
+expressServer.use('/offer', offerRoutes)
+expressServer.use('/user', userRoutes)
+expressServer.use('/upload', uploadRoutes)
+expressServer.use('/identify', identifyRoutes)
+expressServer.use('/chat', chatRoutes)
+expressServer.use('/statistics', statisticsRoutes)
 
 expressServer.listen(process.env.REST_API_PORT, () => {
-    console.log(`Server running on port ${process.env.REST_API_PORT}`);
-    // const backgroundJob = new CronJob('0 0  * * *',  clean());
-    // backgroundJob.start();
-});
+  console.log(`Server running on port ${process.env.REST_API_PORT}`)
+  // const backgroundJob = new CronJob('0 0  * * *',  clean());
+  // backgroundJob.start();
+})
